6bb269a635fd9fd023859f06e7030f89a21fffcf
Pratheepan Bernier <108440591+PratheepanBernier@users.noreply.github.com>
2022-11-16 21:30:38 +0530

    Add files via upload

---
+++ Query1.sql
@@ -0,0 +1,9 @@
+#Write a SQL statement to create a table named countries including columns country_id, country_name and region_id and make sure that no countries except Italy, India and China will be entered in the table.
+
+CREATE DATABASE PRM_special;
+USE PRM_special;
+CREATE TABLE countries (
+    country_id VARCHAR(2),
+    country_name VARCHAR(40) CHECK (country_name IN ('Italy' , 'India', 'China')),
+    region_id DECIMAL(10 , 0 )
+);
